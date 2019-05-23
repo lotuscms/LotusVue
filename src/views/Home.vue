76003a4895d3c904621f7843c9d1917fd2dc6ffb
Casey Clayton <claytoncasey01@gmail.com>
2019-05-23 00:43:09 -0400

    Added recent news section template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="wrapper">
-    <parallax class="section page-header header-filter" parallax-active="true" :style="headerStyle">
+    <parallax
+      class="section page-header header-filter"
+      parallax-active="true"
+      :style="headerStyle"
+    >
       <div class="container">
         <div class="md-layout">
           <div class="md-layout-item md-size-50 md-small-size-100">
@@ -12,7 +16,7 @@
               other information that you deem necessary to highlight your wow
               server.
             </h4>
-            <br>
+            <br />
           </div>
         </div>
       </div>
@@ -21,7 +25,9 @@
       <div class="section">
         <div class="container">
           <div class="md-layout">
-            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto text-center">
+            <div
+              class="md-layout-item md-size-66 md-small-size-100 mx-auto text-center"
+            >
               <h2 class="title text-center">Server Status</h2>
               <!-- <h5 class="description">
               </h5>-->
@@ -37,7 +43,9 @@
                   <div class="icon icon-danger" v-else>
                     <md-icon>cancel</md-icon>
                   </div>
-                  <h4 class="info-title">{{ serverOnline ? 'Online' : 'Offline' }}</h4>
+                  <h4 class="info-title">
+                    {{ serverOnline ? 'Online' : 'Offline' }}
+                  </h4>
                   <p>Server Uptime: {{ serverUptime }}</p>
                 </div>
               </div>
@@ -46,8 +54,13 @@
                   <div class="icon icon-info">
                     <md-icon>group</md-icon>
                   </div>
-                  <h4 class="info-title">Players Online: {{ totalPlayersOnline }}</h4>
-                  <p>Alliance: {{ alliancePlayersOnline }} | Horde: {{ hordePlayersOnline }}</p>
+                  <h4 class="info-title">
+                    Players Online: {{ totalPlayersOnline }}
+                  </h4>
+                  <p>
+                    Alliance: {{ alliancePlayersOnline }} | Horde:
+                    {{ hordePlayersOnline }}
+                  </p>
                 </div>
               </div>
               <div class="md-layout-item md-size-33 md-small-size-100">
@@ -56,9 +69,41 @@
                     <md-icon>memory</md-icon>
                   </div>
                   <h4 class="info-title">Patch</h4>
-                  <p v-for="(val, key) in patchVersions" :key="key">{{ key.charAt(0).toUpperCase() + key.substring(1) }} Patch: {{ val }}</p>
+                  <p v-for="(val, key) in patchVersions" :key="key">
+                    {{ key.charAt(0).toUpperCase() + key.substring(1) }} Patch:
+                    {{ val }}
+                  </p>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section text-center">
+        <div class="container">
+          <h2 class="title">Recent News</h2>
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-50 md-small-size-100"
+              v-for="post in blogPosts"
+              :key="post.id"
+            >
+              <full-bg-card :card-image="post.img">
+                <template slot="cardContent">
+                  <h6 class="card-category text-info">{{ post.category }}</h6>
+                  <a href="javascript:void(0)">
+                    <h3 class="card-title">{{ post.title }}</h3>
+                  </a>
+                  <p class="card-description">{{ post.description }}</p>
+                  <!-- Should be a router link to the specific post -->
+                  <md-button
+                    href="javascript:void(0)"
+                    class="md-danger md-round"
+                  >
+                    <md-icon>format_align_left</md-icon>Read Article
+                  </md-button>
+                </template>
+              </full-bg-card>
             </div>
           </div>
         </div>
@@ -69,11 +114,14 @@
 
 <script>
 import Mixins from '@/plugins/basicMixins';
+import { FullBgCard } from '@/components';
 
 export default {
   name: 'AppLanding',
   mixins: [Mixins.HeaderImage],
-  components: {},
+  components: {
+    FullBgCard
+  },
   bodyClass: 'landing-page',
   data() {
     return {
@@ -86,11 +134,28 @@ export default {
       patchVersions: {
         server: 'v1.0.0',
         launcher: 'v0.0.5'
-      }
+      },
+      blogPosts: [
+        {
+          id: 1,
+          img: require('@/assets/img/examples/card-blog1.jpg'),
+          title: 'Upcoming Races',
+          description:
+            'We have two new races coming to our server, complete with custom starting areas, quests, and ...',
+          category: 'Development'
+        },
+        {
+          id: 2,
+          img: require('@/assets/img/examples/card-blog2.jpg'),
+          title: 'Auction House Bot',
+          description:
+            'Our development team is currently hard at work implementing a fully working Auction ...',
+          category: 'Development'
+        }
+      ]
     };
   }
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
